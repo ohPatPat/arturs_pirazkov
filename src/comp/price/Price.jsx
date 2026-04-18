0f65e337b82803c86bc848b01683b13250297bd6
Patrik <patr5009@elev.techcollege.dk>
2026-04-19 00:14:01 +0200

    Piser Update

--- src/comp/price/Price.jsx
+++ src/comp/price/Price.jsx
@@ -5,28 +5,36 @@ export const Price = (props) => {
     <section id="Priser">
         <h2>Priser</h2>
       <article>
-        <h3>Portræt priser Pr. foto</h3>
+        <h3>Lille pakke</h3>
         <ul>
-          <li> Mit studie: 250kr</li>
+          <li> 10 redigerede billeder</li>
           <hr />
-          <li> På lokation: 280kr + transport</li>
+          <li> Pris: 850 kr.</li>
         </ul>
       </article>
-      <article>
-        <h3>Galleri priser Pr. foto</h3>
+      <article className="PriserSpace">
+        <h3>Mellem pakke</h3>
         <ul>
-          <li>30/40cm: 385kr</li>
+          <li> 25 redigerede billeder</li>
           <hr />
-          <li> 50/70cm: 560kr</li>
-          <hr />
-          <li>60/80cm: 850kr</li>
+          <li> Pris: 1.350 kr.</li>
         </ul>
-        <p>
+      </article>
+            <article className="PriserSpace">
+        <h3>Stor pakke</h3>
+        <ul>
+          <li> 35 redigerede billeder</li>
+          <hr />
+          <li> Pris: 1.650 kr.</li>
+        </ul>
+                <p>
           Oplev skønheden med vores højt kvalitet papir med de udsøgte billeder,
           der sikrer, at hver eneste detalje kommer til.
           Samtidig nyd bekvemmeligheden ved <span> inkluderet fragt</span>.
         </p>
+
       </article>
+
 
     </section>
   );
